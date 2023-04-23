23ed952c9b25ce243e741c0293d1d6b1658e3407
SURAMERICANA\edwaromo <edwardrm81@gmail.com>
2023-04-23 03:01:46 -0500

    Pr ahora se maneja transaccional anotando los metodos Serices con @Transactiona, pero como eso rompe lso conceptos de arquitectura Hexagonal, mas tarde se usará un aspecto para solucionar este problema:

--- src/main/java/com/edw/demo/application_usecase/PersonService.java
+++ src/main/java/com/edw/demo/application_usecase/PersonService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edw.demo.domain.entity.Person;
 import com.edw.demo.domain.entity.PersonUser;
@@ -21,46 +23,55 @@ public class PersonService implements IPersonServiceInputPort {
 	private IPersonRepositoryOutputPort personRepository;
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 	public List<Person> getPersonListAll() {
 		return personRepository.findAll();
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 	public Person getPersonById(Integer id) {
 		return personRepository.findById(id);
 	}
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public Person insertPerson(Person person) {
 		return personRepository.create(person);
 	}
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public Person updatePerson(Person person) {
 		return personRepository.update(person);
 	}
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void deletePersonById(Integer id) {
 		personRepository.deleteById(id);
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 	public List<Person> getPersonByIdAndName(Integer id, String name) {
 		return personRepository.findByIdAndName(id, name);
 	}
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 	public List<Person> getListByIdAndName(Integer id, String name) {
 		return personRepository.getListByIdAndName(id, name);
 	}
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 	public List<Person> getListByUserLogin(String login) {
 		return personRepository.getListByUserLogin(login);
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
 	public List<PersonUser> getPersonUserListByUserLogin(String login) {
 		return personRepository.getPersonUserListByUserLogin(login);
 	}
--- src/main/java/com/edw/demo/infra/outputadapter_drivenadapter/db/jpa/extendsinterfaces/IPersonJPA.java
+++ src/main/java/com/edw/demo/infra/outputadapter_drivenadapter/db/jpa/extendsinterfaces/IPersonJPA.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.edw.demo.domain.entity.Person;
 import com.edw.demo.infra.outputadapter_drivenadapter.db.jpa.entity.PersonJpaEntity;
 
 public interface IPersonJPA extends JpaRepository <PersonJpaEntity, Integer>{ 
