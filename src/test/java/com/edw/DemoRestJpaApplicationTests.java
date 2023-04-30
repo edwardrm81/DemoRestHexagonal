9e177099530b9115b9cf150705bf1ffa976b6e77
SURAMERICANA\edwaromo <edwardrm81@gmail.com>
2023-04-30 03:38:50 -0500

    Deshabilitar test

--- src/test/java/com/edw/DemoRestJpaApplicationTests.java
+++ src/test/java/com/edw/DemoRestJpaApplicationTests.java
@@ -1,12 +1,14 @@
 package com.edw;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoRestJpaApplicationTests {
 
-	@Test
+	//@Test
+	@Disabled
 	void contextLoads() {
 	}
 
